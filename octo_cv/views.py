19feef1aecd373208f597d9ee2b85257b921ad98
marianitadn <maria.nita.dn@gmail.com>
2013-11-02 20:39:31 +0200

    Transform home view from function to class. Implement get request method

--- octo_cv/views.py
+++ octo_cv/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-def home(self):
-    return HttpResponse()
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+    def get(self, request):
+        html = "<h1> Buna! </h1>"
+        return HttpResponse(html)
--- octo_site/urls.py
+++ octo_site/urls.py
@@ -1,17 +1,11 @@
 from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+
+from octo_cv.views import HomeView
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'octo_cv.views.home', name='home'),
-    # url(r'^octo_site/', include('octo_site.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+    url(r'^$', HomeView.as_view(), name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
