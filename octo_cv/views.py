e17bd959ec4c8c0d42fc28a8520370aa32c42f96
marianitadn <maria.nita.dn@gmail.com>
2013-11-09 18:00:14 +0200

    Add home image

--- octo_cv/views.py
+++ octo_cv/views.py
@@ -10,7 +10,8 @@ class HomeView(TemplateView):
     template_name = 'home.html'
 
     def get(self, request):
-        return render(request, self.template_name)
+        home = Contact.objects.get(name='Home')
+        return render(request, self.template_name, {'home': home})
 
 class ContactView(TemplateView):
     template_name = 'contact.html'
