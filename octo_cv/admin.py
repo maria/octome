ef12015627fe4bc59da4a5b1d7e8ec0316009e86
marianitadn <maria.nita.dn@gmail.com>
2013-11-09 18:34:15 +0200

    Add Education admin

--- octo_cv/admin.py
+++ octo_cv/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from octo_cv.models.contact import Contact
+from octo_cv.models.education import Education
 
 
 class ContactAdmin(admin.ModelAdmin):
@@ -8,4 +9,10 @@ class ContactAdmin(admin.ModelAdmin):
     search_fields = ('name', 'type')
 
 
+class EducationAdmin(admin.ModelAdmin):
+    list_display = ('school', 'degree', 'field')
+    search_fields = ('school', 'degree', 'field', 'country')
+
+
 admin.site.register(Contact, ContactAdmin)
+admin.site.register(Education, EducationAdmin)
