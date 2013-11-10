701cda61b7fd182ca6f0ee8436450edd456e577e
marianitadn <maria.nita.dn@gmail.com>
2013-11-10 11:44:00 +0200

    Refactor social widgets

--- octo_cv/views.py
+++ octo_cv/views.py
@@ -16,8 +16,17 @@ class ContactView(TemplateView):
     template_name = 'contact.html'
 
     def get(self, request):
+        """Get all the Contacts of type Social Profile and create a mapping:
+            social_contact_name -> social_contact_object,
+        to display them in a given order in the view.
+        """
+        view_social_contacts = dict()
         social_contacts = Contact.objects.filter(type=ContactType.SOCIAL)
-        return render(request, self.template_name, {'social_contacts': social_contacts})
+
+        for social_contact in social_contacts:
+            view_social_contacts[social_contact.name.lower()] = social_contact
+        return render(request, self.template_name,
+                      {'social_contacts': view_social_contacts})
 
 
 class WorkView(TemplateView):
