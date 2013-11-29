0dde6e73e326c7bbf0c8e4d8fedbedbcd390b7fb
marianitadn <maria.nita.dn@gmail.com>
2013-11-29 21:26:28 +0200

    Add Project Admin model

--- octo_cv/admin.py
+++ octo_cv/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from octo_cv.models import Contact, Education, Work
+from octo_cv.models import Contact, Education, Work, Project
 
 
 class ContactAdmin(admin.ModelAdmin):
@@ -12,11 +12,17 @@ class EducationAdmin(admin.ModelAdmin):
     list_display = ('school', 'degree', 'field')
     search_fields = ('school', 'degree', 'field', 'country')
 
+
 class WorkAdmin(admin.ModelAdmin):
     list_display = ('id', 'company', 'role', 'type')
     search_fields = ('type', 'company', 'role', 'city')
 
 
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'url', 'type')
+
+
 admin.site.register(Contact, ContactAdmin)
 admin.site.register(Education, EducationAdmin)
 admin.site.register(Work, WorkAdmin)
+admin.site.register(Project, ProjectAdmin)
--- octo_cv/models/__init__.py
+++ octo_cv/models/__init__.py
@@ -1,3 +1,4 @@
 from contact import Contact
 from education import Education
 from work import Work
+from project import Project
